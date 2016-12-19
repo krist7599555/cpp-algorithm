f9d4dde7a674085568bcadbddd29e33ec7924f37
krist7599555 <krist_7599555@hotmail.com>
2016-12-19 10:41:21 +0700

    Update power logN with out recursive.cpp

--- power logN with out recursive.cpp
+++ power logN with out recursive.cpp
@@ -2,7 +2,7 @@ typedef long long lli;
 lli pow(lli num, lli factor, lli modd = 1e9+7){
 	
 	// 	1	0 	0 	1 	0 	0 	1 	1 	1
-	// 	n^9	n^8 n^7	n^6	n^5	n^4	n^3	n^2	n^1	
+	// 	n^9	n^8 	n^7	n^6	n^5	n^4	n^3	n^2	n^1	
 	
 	lli result = 1;
 	lli nownum = 1;
