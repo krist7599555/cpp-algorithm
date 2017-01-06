af5c1d65671cfdd1f707e733b66a68c6b7231150
krist7599555 <krist_7599555@hotmail.com>
2017-01-06 13:54:22 +0700

    Update genSequence in compileTime.cpp

--- genSequence in compileTime.cpp
+++ genSequence in compileTime.cpp
@@ -1,18 +1,18 @@
-// used sequencegenerate in " Compile Time ++ " 
+// used sequencegenerate in " Compile Time C++14 " 
 
 #include <bits/stdc++.h>
+#include <utility>
+#include <tuple>
 using namespace std;
 
 template<size_t ... arg>
 void Size_tSequence( index_sequence< arg... >){
-	for(size_t i : { arg... }) 
-		cout << i << ' ' << endl;
+	for(size_t i : { arg... })  cout << i << ' ' << endl;
 };
 
 template<typename Type, Type ... arg > // universal because template a type ans used integer_sequence
 void intSequence( integer_sequence<Type,arg...>){
-	for(Type i : { arg... }) 
-		cout << i << ' ';
+	for(Type i : { arg... })  cout << i << ' ';
 };
 
 template<int T>
@@ -25,17 +25,21 @@ vector<int> generateSquare( integer_sequence<int,T...>){ // spasific type [int]
 
 namespace catalan {
 	
-	typedef size_t sz_t;
+	/* 
+	//still OK
+	template <size_t N> 		struct cata {	       enum { value = cata<N-1> + for_cata<_,_> ... }; };
+	template <size_t N> 		class  cata { public : enum { value = cata<N-1> + for_cata<_,_> ... }; };
+	*/
 	
 	template <size_t N> 	   	struct cata; // declearlation not have spasific <>
-	template <>		 	   		struct cata<0>; // sub declear for determind case
-	template <size_t L,size_t R>struct for_cata;
+	template <>		 	struct cata<0>; // sub declear for determind case
+	template <size_t L,size_t R>	struct for_cata;
 	template <size_t L> 	   	struct for_cata<-1,L>;
 	
 	template <size_t N> 	   	struct cata 	  	: std::integral_constant<size_t, for_cata<0,N-1>::value > {};
-	template <>		 	   		struct cata<0> 		: std::integral_constant<size_t, 1 > {};
+	template <>		 	struct cata<0> 		: std::integral_constant<size_t, 1 > {};
 	
-	template <size_t L,size_t R>struct for_cata 		: std::integral_constant<size_t, cata<L>::value * cata<R>::value + for_cata<L+1,R-1>::value > {};  
+	template <size_t L,size_t R>	struct for_cata 	: std::integral_constant<size_t, cata<L>::value * cata<R>::value + for_cata<L+1,R-1>::value > {};  
 	template <size_t L> 	   	struct for_cata<L,-1> 	: std::integral_constant<size_t, 0LL > {};  
 	
 	template <size_t ... T> // variadic template
