991fac128d85ed7450eebcc045bcec16d1d60123
krist7599555 <krist_7599555@hotmail.com>
2017-04-11 11:48:16 +0700

    Update fenwickRSQ_easy_conecpt.cpp

--- fenwickRSQ_easy_conecpt.cpp
+++ fenwickRSQ_easy_conecpt.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 
 /**
- * 	concept : lazy add, find sum of [1..b] by
+ *  concept : lazy add, find sum of [1..b] by
  *  SiNGle >> if we cut in middle we get  sum value to multiply
- * 	GRouP  >> if mid -> cancle of single else look to another
+ *  GRouP  >> if mid -> cancle of single else look to another
  *  (v * a-1) - (v * b) = v * (b - a - 1) = v * len
  */
 
@@ -32,16 +32,16 @@ public:
 	Fenwick(int n): n(n), sng(new T[n+1]()), grp(new T[n+1]()) {}
 	
 	void update (int l,int r, T v){
-		update (sng, l  ,  v);
-		update (sng, r+1, -v);
-		update (grp, l  ,  v * (l - 1));
-		update (grp, r+1, -v * (r    ));
+		update (sng, l  ,  v); // + 1.) if cut mid found this == query_pos * v
+		update (sng, r+1, -v); // - 2.) can cancle 1.) if > query_pos and used 3.) 4.) only
+		update (grp, l  ,  v * (l - 1)); // + 3.) first range can cancle 1.) if cut mid
+		update (grp, r+1, -v * (r    )); // - 4.) combine with 3.) = get full range
 	}
 	
 	T query (int l, int r){
 		l--;
-		int suml = query(sng,l) * l - query(grp,l);
-		int sumr = query(sng,r) * r - query(grp,r);
+		int suml = query(sng,l) * l - query(grp,l); // sum[1..l]
+		int sumr = query(sng,r) * r - query(grp,r); // sum[1..r]
 		return sumr - suml;
 	}
 	
