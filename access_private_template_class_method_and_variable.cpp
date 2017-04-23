86bc50a3d717135971b28b9fc8f1ddf82f188620
krist7599555 <krist_7599555@hotmail.com>
2017-04-23 22:36:05 +0700

    Create access_private_template_class_method_and_variable.cpp

---
+++ access_private_template_class_method_and_variable.cpp
@@ -0,0 +1,69 @@
+#include <bits/extc++.h>
+#include <iostream>
+#define rep(i,k,n) for(decltype(k) i=(k);i!=(n);++i)
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) int((x).size())
+#define fi first
+#define se second
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds::detail;
+using namespace std;
+typedef long long lli;
+typedef std::vector<int> Vec;
+typedef std::pair<int,int> pii;
+
+template<typename Tag>
+struct result {
+  /* export it ... */
+  typedef typename Tag::type type;
+  static type ptr;
+};
+
+template<typename Tag>
+typename result<Tag>::type result<Tag>::ptr;
+
+template<typename Tag, typename Tag::type p>
+struct rob : result<Tag> {
+  /* fill it ... */
+  struct filler {
+    filler() { result<Tag>::ptr = p; }
+  };
+  static filler filler_obj;
+};
+
+template<typename Tag, typename Tag::type p>
+typename rob<Tag, p>::filler rob<Tag, p>::filler_obj;
+
+template <typename T>
+struct A {
+public:
+	int p_val = 0;
+private:
+	int val = 0;
+	void f() {
+		std::cout << "proof! " << " private: "<< val << "  public: " << p_val << std::endl;
+	}
+};
+
+typedef  int A<int>::*poiter_member_type_int;
+typedef  void(A<int>::*poiter_method_type)(void);
+
+template <typename T>
+struct Af { typedef void(A<T>::*type)(); };
+
+template <typename T>
+struct Am { typedef int A<T>::*type; };
+
+template class rob<Af<int>, &A<int>::f  >;
+template class rob<Am<int>, &A<int>::val>;
+
+int main() {
+  A<int> a;
+  int A<int>::*      poi_member  = &A<int>::p_val;
+  poiter_member_type_int poi_member2 = &A<int>::p_val;
+  (a.*poi_member) = 13;
+  (a.*result<Af<int>>::ptr)();
+  (a.*poi_member2) = 23;
+  (a.*result<Af<int>>::ptr)();
+}
