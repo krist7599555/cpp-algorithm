ca4703833acb912217a55d462676541829f23ab1
Krist Pornpairin <krist_7599555@hotmail.com>
2017-09-02 08:41:13 +0700

    Create linux_memory_used.cpp

---
+++ linux_memory_used.cpp
@@ -0,0 +1,9 @@
+#include <ios>
+long process_size_in_pages(void){
+   long s = -1;
+   FILE *f = fopen("/proc/self/statm", "r");
+   if(f)
+      fscanf(f, "%ld", &s),
+  	  fclose(f);
+   return s;
+}
